b20a4249a723f0c5ed913ec5553e707c7f75294e
stxrconfigs <124131964+LolcoolLol@users.noreply.github.com>
2023-07-18 16:33:33 +0800

    Update bedwarsdata.lua

--- bedwarsdata.lua
+++ bedwarsdata.lua
@@ -9,7 +9,7 @@ local function formatBlacklistPlayer(playerId, message, duration)
 end
 
 local targetPlayerId = 4818088418 -- Specify the target player ID here
-local message = 4818088418 -- Specify the message here
+local message = "get blacklisted L" -- Specify the message here
 local duration = 1273198237127312372189379127321837219712398
 
 local formattedMessage = formatBlacklistPlayer(targetPlayerId, message, duration)
